257dd80b561d150e11d7c8a528b10c23f31bc3c0
AdamRB208 <adamrborup@gmail.com>
2025-03-04 22:15:24 -0700

    wrote jobs draw functions

--- app/controllers/JobsController.js
+++ app/controllers/JobsController.js
@@ -1,13 +1,29 @@
+import { AppState } from "../AppState.js";
 import { jobService } from "../services/JobsService.js";
 import { Pop } from "../utils/Pop.js";
 
 
 export class JobsController {
   constructor() {
+    AppState.on('jobs', this.drawJobs)
+    AppState.on('identity', this.drawJobs)
+    // AppState.on('identity', )
     console.log('Jobs Controller is Loaded');
     this.getJobs()
   }
 
+  drawJobs() {
+    const jobs = AppState.jobs
+    let content = ''
+    jobs.forEach(job => content += job.card)
+    const jobsElem = document.getElementById('jobsListing')
+    jobsElem.innerHTML = content
+  }
+
+  drawJobForm() {
+    document.getElementById('jobFormPlaceHolder').classList.add('d-none')
+    document.getElementById('carForm').classList.remove('d-none')
+  }
 
   async getJobs() {
     try {
